8241e6b0171d17ebc79901b7a2331f9e59bc4420
Ricardo Solano <richard.solano@gmail.com>
2017-04-16 14:29:38 -0400

    Refactor clone.py

--- clone.py
+++ clone.py
@@ -14,16 +14,22 @@ IMAGE_PATH = DATA_DIR + 'IMG/'
 IMAGE_SEP = '\\'
 STEERING_CORRECTION = 0.25
 
-print("loading csv data...")
-lines = []
-with open(DATA_DIR + 'driving_log.csv') as csvfile:
-    reader = csv.reader(csvfile)
-    for line in reader:
-        lines.append(line)
+def load_data(filename="driving_log.csv"):
+    print("loading csv data...")
+    lines = []
+    with open(DATA_DIR + filename) as csvfile:
+        reader = csv.reader(csvfile)
+        for line in reader:
+            lines.append(line)
 
-train_samples, validation_samples = train_test_split(lines, test_size=0.2)
+    return lines
 
 def generator(samples, batch_size=32):
+    """
+    Generate batch samples and yield on the fly. Load center, left and right
+    images along with their corresponding angle measurements. Augment samples
+    by flipping each image and inverting their angle values.
+    """
     num_samples = len(samples)
     while 1:
         shuffle(samples)
@@ -60,26 +66,41 @@ def generator(samples, batch_size=32):
             y_train = np.array(angles)
             yield shuffle(X_train, y_train)
 
-train_generator = generator(train_samples, batch_size=32)
-validation_generator = generator(validation_samples, batch_size=32)
+def nvidia_architecture():
+    """
+    Nvidia architecture implementation using Keras. The model consists of 4
+    convolutions and 5 fully connected layers.
+    Preprocessing consists of image pixel normalization and cropping.
+    """
+    model = Sequential()
+    model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
+    model.add(Cropping2D(cropping=((70,25), (0,0))))
+    model.add(Convolution2D(24,5,5, subsample=(2,2), activation="relu"))
+    model.add(Convolution2D(36,5,5, subsample=(2,2), activation="relu"))
+    model.add(Convolution2D(48,5,5, subsample=(2,2), activation="relu"))
+    model.add(Convolution2D(64,5,5, activation="relu"))
+    model.add(Flatten())
+    model.add(Dense(100))
+    model.add(Dense(50))
+    model.add(Dense(10))
+    model.add(Dense(1))
 
-model = Sequential()
-model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
-model.add(Cropping2D(cropping=((70,25), (0,0))))
-model.add(Convolution2D(24,5,5, subsample=(2,2), activation="relu"))
-model.add(Convolution2D(36,5,5, subsample=(2,2), activation="relu"))
-model.add(Convolution2D(48,5,5, subsample=(2,2), activation="relu"))
-model.add(Convolution2D(64,5,5, activation="relu"))
-model.add(Flatten())
-model.add(Dense(100))
-model.add(Dense(50))
-model.add(Dense(10))
-model.add(Dense(1))
-model.compile(loss='mse', optimizer='adam')
+    return model
 
-print("training...")
-model.fit_generator(train_generator, samples_per_epoch=len(train_samples),\
-        validation_data=validation_generator, nb_val_samples=len(validation_samples), nb_epoch=5)
+def train(model, epochs=5):
+    print("training...")
+    model.compile(loss='mse', optimizer='adam')
+    model.fit_generator(train_generator, samples_per_epoch=len(train_samples),\
+            validation_data=validation_generator, nb_val_samples=len(validation_samples), nb_epoch=epochs)
 
-print("save")
-model.save('model.h5')
+    print("save")
+    model.save('model.h5')
+
+if __name__ == '__main__':
+    samples = load_data()
+    train_samples, validation_samples = train_test_split(samples, test_size=0.2)
+    train_generator = generator(train_samples, batch_size=32)
+    validation_generator = generator(validation_samples, batch_size=32)
+
+    model = nvidia_architecture()
+    train(model, epochs=5)
