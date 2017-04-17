8d96a512daacf191f94856d683379dfac176f5b2
Ricardo Solano <richard.solano@gmail.com>
2017-04-16 22:29:24 -0400

    Crop image before normalization. Add Dropout layers to reduce overfitting

--- clone.py
+++ clone.py
@@ -7,7 +7,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda, Convolution2D, Cropping2D
+from keras.layers import Flatten, Dense, Lambda, Convolution2D, Cropping2D, Dropout
 
 DATA_DIR = 'data6/'
 IMAGE_PATH = DATA_DIR + 'IMG/'
@@ -74,16 +74,19 @@ def nvidia_architecture():
     well as image cropping.
     """
     model = Sequential()
-    model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
-    model.add(Cropping2D(cropping=((70,25), (0,0))))
+    model.add(Cropping2D(cropping=((70,25), (0,0)), input_shape=(160,320,3)))
+    model.add(Lambda(lambda x: x / 255.0 - 0.5))
     model.add(Convolution2D(24,5,5, subsample=(2,2), activation="relu"))
     model.add(Convolution2D(36,5,5, subsample=(2,2), activation="relu"))
     model.add(Convolution2D(48,5,5, subsample=(2,2), activation="relu"))
     model.add(Convolution2D(64,5,5, activation="relu"))
     model.add(Flatten())
     model.add(Dense(100))
+    model.add(Dropout(0.5))
     model.add(Dense(50))
+    model.add(Dropout(0.5))
     model.add(Dense(10))
+    model.add(Dropout(0.5))
     model.add(Dense(1))
 
     return model
